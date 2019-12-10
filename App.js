9d0536feda0053d57b5e3945d9f56aefdeb651e4
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-12-10 14:09:46 +0200

    added the landing component

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+
+import { Landing } from './screens/Landing';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Landing /> 
+    </SafeAreaView>
   );
 }
 
