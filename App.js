4d591269f69211daeebe3b5f9fba7129912fcaef
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-12-10 20:27:11 +0200

    exported navigation in the app Relates #2

--- App.js
+++ App.js
@@ -2,24 +2,24 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import Constants from 'expo-constants';
 
-import { SettingsScreen } from './screens/SettingsScreen';
+import AppNavigation from './navigation/AppNavigation';
 
 class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <SettingsScreen />
+        <AppNavigation />
       </View>
     );
-  }
+  } 
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#e3e3e4',
+    backgroundColor: '#e3e3e4',  
     marginTop: Constants.statusBarHeight,
-    padding: 20
+    // padding: 20
   }
 });
 
