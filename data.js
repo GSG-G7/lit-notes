9bd8a1e1273f049c4fa2ceb25eb8cc9309f67d2f
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-12-10 15:24:05 +0200

    added fake data for use

---
+++ data.js
@@ -0,0 +1,32 @@
+export const notes = [
+    {
+        id: 1,
+        title: 'This is a title',
+        desc: 'This is a long short description that is going to be in the card',
+        timestamp: Date.now().toString()
+    },
+    {
+        id: 2,
+        title: 'This is a title',
+        desc: 'This is a long short description that is going to be in the card',
+        timestamp: Date.now().toString()
+    },
+    {
+        id: 3,
+        title: 'This is a title',
+        desc: 'This is a long short description that is going to be in the card',
+        timestamp: Date.now().toString()
+    },
+    {
+        id: 4,
+        title: 'This is a title',
+        desc: 'This is a long short description that is going to be in the card',
+        timestamp: Date.now().toString()
+    },
+    {
+        id: 5,
+        title: 'This is a title',
+        desc: 'This is a long short description that is going to be in the card',
+        timestamp: Date.now().toString()
+    },
+]
