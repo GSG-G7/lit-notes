b59d5f3d8f67798fda5ac46cf02a9dae51fae296
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-12-10 16:19:40 +0200

    modefied components styles

--- Components/SearchBox.jsx
+++ Components/SearchBox.jsx
@@ -10,6 +10,7 @@ export const SearchBox = ({ value, handler }) => {
       <Ionicons name="md-search" size={25} style={styles.searchIcon} />
       <TextInput
         placeholder="search"
+        placeholderTextColor={colors.blue}
         value={value}
         onChangeText={handler}
         style={styles.searchText}
@@ -24,12 +25,27 @@ const styles = StyleSheet.create({
     padding: 10,
     flexDirection: 'row',
     justifyContent: 'flex-start',
+    color: colors.blue,
+    shadowColor: '#aaa',
+    shadowOffset: {
+      width: 0,
+      height: 3
+    },
+    shadowOpacity: 0.34,
+    shadowRadius: 5,
+
+    elevation: 10,
+    borderRadius: 5,
     color: colors.blue
   },
   searchIcon: {
-    marginRight: 5
+    marginRight: 5,
+    color: colors.blue
+
   },
   searchText: {
-    fontSize: 16
+    fontSize: 16,
+    width: '100%',
+    color: colors.blue
   }
 });
