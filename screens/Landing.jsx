9eacc36ab6ff1c4b66dea2aff08998f71f109c9f
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-12-10 15:25:59 +0200

    used the main button in landing

--- screens/Landing.jsx
+++ screens/Landing.jsx
@@ -1,41 +1,29 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableWithoutFeedback } from 'react-native';
+import { MainButton } from '../Components/MainButton';
 
 export const Landing = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.heading}>LitNotes</Text>
       <Text style={styles.about}>Lorem ipsum dolor sit amet.</Text>
-      <TouchableWithoutFeedback onPress={() => alert('clicked')}>
-          <Text style={styles.button}>Google Sign In</Text>
-      </TouchableWithoutFeedback>
+      <MainButton title="Google Sign In" handler={() => alert('clicked')} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        // flex: 1,
-        alignItems: 'center'
-    },
-    heading: {
-        fontSize: 32,
-        color: '#242424',
-        fontWeight: '700'
-    },
-    about: {
-        fontSize: 16,
-        color: '#acacad',
-        marginTop: 10,
-    },
-    button: {
-        backgroundColor: '#f3f3f3',
-        color: '#242424',
-        fontSize: 16,
-        padding: 12,
-        textAlign: 'center',
-        fontWeight: '700',
-        marginTop: 20,
-        borderRadius: 12
-    }
-})
+  container: {
+    alignItems: 'center'
+  },
+  heading: {
+    fontSize: 32,
+    color: '#242424',
+    fontWeight: '700'
+  },
+  about: {
+    fontSize: 16,
+    color: '#acacad',
+    marginTop: 10
+  }
+});
