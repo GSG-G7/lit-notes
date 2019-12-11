e9c27a502a2c1e06a90b535ebc91246fa56edccb
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-12-11 15:10:17 +0200

    added another screen and made a stack

--- navigation/AppNavigation.js
+++ navigation/AppNavigation.js
@@ -1,19 +1,32 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
-import  LandingScreen  from '../screens/LandingScreen';
-import { AuthLoadingScreen } from '../screens/AuthLoadingScreen';
+import LandingScreen from '../screens/LandingScreen';
+import SignUpScreen from '../screens/SignUpScreen';
+import AuthLoadingScreen from '../screens/AuthLoadingScreen';
 import MainStack from './MainTabNavigation';
+
+const LandingStack = createStackNavigator(
+  {
+    Login: LandingScreen,
+    SignUp: SignUpScreen
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      header: navigation.state.routeName === 'Login' ? null : false
+    })
+  }
+);
 
 export default createAppContainer(
   createSwitchNavigator(
     {
       AuthLoading: AuthLoadingScreen,
       Main: MainStack,
-      Landing: LandingScreen 
+      Landing: LandingStack
     },
     {
       initialRouteName: 'AuthLoading'
-    },
+    }
   )
 );
