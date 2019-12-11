3e564ff14b9d8dbd1ed3c38d9389da9425d699d0
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-12-11 09:55:13 +0200

    removed unneeded stack

--- navigation/AppNavigation.js
+++ navigation/AppNavigation.js
@@ -5,16 +5,12 @@ import { LandingScreen } from '../screens/LandingScreen';
 import { AuthLoadingScreen } from '../screens/AuthLoadingScreen';
 import MainStack from './MainTabNavigation';
 
-// const LandingStack = createStackNavigator({ Landing: LandingScreen });
-
-// LandingStack.headerMode = 'none';
-
 export default createAppContainer(
   createSwitchNavigator(
     {
       AuthLoading: AuthLoadingScreen,
       Main: MainStack,
-      Landing: LandingScreen
+      Landing: LandingScreen 
     },
     {
       initialRouteName: 'AuthLoading'
