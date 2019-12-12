4119c1805c3435b92fe6b0c12639f18f26f1fc72
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-12-12 10:02:10 +0200

    added different style for the add

--- navigation/MainTabNavigation.js
+++ navigation/MainTabNavigation.js
@@ -4,11 +4,10 @@ import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { Ionicons } from '@expo/vector-icons';
 import { View } from 'react-native';
 
-import { HomeScreen } from '../screens/HomeScreen';
-import  AddScreen  from '../screens/AddScreen';
-import SettingsScreen  from '../screens/SettingsScreen';
+import HomeScreen from '../screens/HomeScreen';
+import AddScreen from '../screens/AddScreen';
+import SettingsScreen from '../screens/SettingsScreen';
 import { colors } from '../Constants/Colors';
-
 
 export default MainTabNavigator = createBottomTabNavigator(
   {
@@ -20,13 +19,20 @@ export default MainTabNavigator = createBottomTabNavigator(
     defaultNavigationOptions: ({ navigation }) => ({
       tabBarIcon: ({ focused, horizontal, tintColor }) => {
         const { routeName } = navigation.state;
-        let iconName;
+        let iconName, style;
         switch (routeName) {
           case 'Home':
             iconName = 'md-home';
             break;
           case 'Add':
             iconName = 'md-add';
+            style = {
+              backgroundColor: colors.blue,
+              padding: 5,
+              paddingHorizontal: 12,
+              color: '#fff',
+              borderRadius: 5
+            };
             break;
           case 'Settings':
             iconName = 'md-settings';
@@ -34,10 +40,12 @@ export default MainTabNavigator = createBottomTabNavigator(
           default:
             break;
         }
-        return <Ionicons name={iconName} size={25} color={tintColor} />;
+        return (
+          <Ionicons name={iconName} size={25} color={tintColor} style={style} />
+        );
       },
       tabBarLabel: <View />,
-      tabBarVisible: () => navigation.state.routeName === 'Add' ? false : true
+      tabBarVisible: navigation.state.routeName === 'Add' ? false : true
     }),
     tabBarOptions: {
       activeTintColor: colors.blue,
